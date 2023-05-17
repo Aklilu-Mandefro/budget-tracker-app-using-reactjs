e7908fd01ef1272e37ae51dd0dcf2f36173945ad
Aklilu Mandefro <aklilu.mandefro@volunteer-tech.com>
2023-05-17 01:21:40 -0700

    Comments added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+// Imports the bootstrap CSS into the project
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+//Create a component that displays "Hello React!" with paragraph tags
 const App = () => {
 	return <p>Hello React!</p>;
 };
 
+//Export the previous component so other components can use it
 export default App;
