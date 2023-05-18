42112f5c508731d847078ec36fbf857ff430f7fc
Aklilu Mandefro <aklilu.mandefro@volunteer-tech.com>
2023-05-18 01:40:37 -0700

    Spent so Far component created

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,12 @@
+// create the Spent so Far component, which shows how much the user has spent so far
+import React from 'react';
+
+const ExpenseTotal = () => {
+	return (
+		<div className='alert alert-primary'>
+			<span>Spent so far: £1000</span>
+		</div>
+	);
+};
+
+export default ExpenseTotal;
