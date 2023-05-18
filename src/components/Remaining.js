6c1274b6c564dc7a7b797c8f439cb719948ffba4
Aklilu Mandefro <aklilu.mandefro@volunteer-tech.com>
2023-05-18 01:36:21 -0700

    Remaining component created

---
+++ src/components/Remaining.js
@@ -0,0 +1,13 @@
+//create the Remaining component, which shows how much budget the user has left.
+
+import React from 'react';
+
+const Remaining = () => {
+	return (
+		<div className='alert alert-success'>
+			<span>Remaining: £1000</span>
+		</div>
+	);
+};
+
+export default Remaining;
