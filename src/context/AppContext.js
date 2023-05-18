ca2a30c41ce3c2962ea835da6b0f565d41f6f74b
Aklilu Mandefro <aklilu.mandefro@volunteer-tech.com>
2023-05-18 04:46:49 -0700

    AppContext file updated

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,6 +5,13 @@ const AppReducer = (state, action) => {
 				...state,
 				expenses: [...state.expenses, action.payload],
 			};
+		case 'DELETE_EXPENSE':
+			return {
+				...state,
+				expenses: state.expenses.filter(
+					(expense) => expense.id !== action.payload
+				),
+			};
 		default:
 			return state;
 	}
