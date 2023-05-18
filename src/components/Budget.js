0a093979cb7599d1daf653b7dc97f7ff53a65d9c
Aklilu Mandefro <aklilu.mandefro@volunteer-tech.com>
2023-05-18 01:24:48 -0700

    Budget compponent added

---
+++ src/components/Budget.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//Create a new component called Budget
+const Budget = () => {
+	return (
+		<div className='alert alert-secondary'>
+			<span>Budget: £2000</span>
+		</div>
+	);
+};
+
+export default Budget;
