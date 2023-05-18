3b377f92f17f21d080389289c9c2e9140127ee53
Aklilu Mandefro <aklilu.mandefro@volunteer-tech.com>
2023-05-18 04:31:36 -0700

    AppContext file created

---
+++ src/context/AppContext.js
@@ -0,0 +1,33 @@
+const AppReducer = (state, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+const initialState = {
+	budget: 2000,
+	expenses: [
+		{ id: 12, name: 'shopping', cost: 40 },
+		{ id: 13, name: 'holiday', cost: 400 },
+		{ id: 14, name: 'car service', cost: 50 },
+	],
+};
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+	const [state, dispatch] = useReducer(AppReducer, initialState);
+
+	return (
+		<AppContext.Provider
+			value={{
+				budget: state.budget,
+				expenses: state.expenses,
+				dispatch,
+			}}
+		>
+			{props.children}
+		</AppContext.Provider>
+	);
+};
