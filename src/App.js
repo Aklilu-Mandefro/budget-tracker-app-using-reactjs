1eb8d21bc7c4e0991063376a6d909001489f141c
Aklilu Mandefro <aklilu.mandefro@volunteer-tech.com>
2023-05-18 01:53:51 -0700

    ExpenseList component rendered in App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
 
 const App = () => {
 	return (
-			<div className='container'>
-				<h1 className='mt-3'>My Budget Planner</h1>
-				<div className='row mt-3'>
-					<div className='col-sm'>
-						<Budget />
-					</div>
-					<div className='col-sm'>
-						<Remaining />
-					</div>
-					<div className='col-sm'>
-						<ExpenseTotal />
-					</div>
+		<div className='container'>
+			<h1 className='mt-3'>My Budget Planner</h1>
+			<div className='row mt-3'>
+				<div className='col-sm'>
+					<Budget />
+				</div>
+				<div className='col-sm'>
+					<Remaining />
+				</div>
+				<div className='col-sm'>
+					<ExpenseTotal />
 				</div>
 			</div>
+			<h3 className='mt-3'>Expenses</h3>
+			<div className='row mt-3'>
+				<div className='col-sm'>
+					<ExpenseList />
+				</div>
+			</div>
+		</div>
 	);
 };
 
